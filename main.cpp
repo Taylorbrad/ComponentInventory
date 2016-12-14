ec6972b4b4f5435312feb9003218d5563a895b54
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-12-14 15:10:10 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int menuSelection = 0;
+    
+    cout << "----Menu----\n";
+    cout << "1: Display All Components\n";
+    cout << "2: Add a Component\n";
+    cout << "3: \n";
+    cout << "4: \n";
+    cin >> menuSelection;
+    
+    switch (menuSelection)
+    {
+        case 1:
+        cout << "
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        default:
+        break;
+        
+    }
+}
+
