adb2e995eb681befce8a1df376b50305f8473c4b
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-12-14 16:26:37 -0700

    Update Component.cpp

--- Component.cpp
+++ Component.cpp
@@ -2,6 +2,17 @@
 
 Component::Component(string id, string description, string type, double retail, double boughtFor) : identifier(id), description(description), componentType(type), retialPrice(retail), boughtPrice(boughtFor)
 {}
-Component::toString()
+string Component::toString()
 {
+  stringstream ss;
+  ss << "ID: " << this->identifier << endl;
+  ss << "Retail Price: $" << this->retailPrice << endl;
+  ss << "Purchase Price: $" << this->boughtPrice << endl;
+  ss << "Quantity: " << this->quantity << endl;
+  
+  return ss.str();
+}
+Component::getComponentType()
+{
+  return this->componentType;
 }
