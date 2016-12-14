c423d57a7b3baf55c467a78fdd523459984c8f9b
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-12-14 15:55:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,30 @@ int main()
     cout << "----Menu----\n";
     cout << "1: Display All Components\n";
     cout << "2: Add a Component\n";
-    cout << "3: \n";
+    cout << "3: Display Specific Component List\n";
+    /*
     cout << "4: \n";
+    cout << "5: \n";
+    */
     cin >> menuSelection;
     
     switch (menuSelection)
     {
         case 1:
-        cout << "
+        cout << endl;
+        cout << "CPUs\n----\n";
+        //TODO: display all CPU's
+        cout << "GPUs\n----\n";
+        //TODO: display all GPUs
+        cout << "Motherboards\n-----------\n";
+        //display mobos
+        cout << "RAM\n---\n";
+        //display rams
+        cout << "Storage\n-------\n";
+        //display storages
+        cout << "CPU Coolers\n----------\n"
+        //display coolers
+        cout << "etc\n";
         break;
         case 2:
         break;
